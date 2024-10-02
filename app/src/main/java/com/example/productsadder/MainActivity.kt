75ff6a3fe9113454925cb96b399bd5bc028a63f0
Nour Allam <nourallam2016@gmail.com>
2024-10-02 17:58:53 +0300

    complete

--- app/src/main/java/com/example/productsadder/MainActivity.kt
+++ app/src/main/java/com/example/productsadder/MainActivity.kt
@@ -1,20 +1,135 @@
 package com.example.productsadder
 
+import android.content.Intent
+import android.content.Intent.ACTION_PICK
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.productsadder.databinding.ActivityMainBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.skydoves.colorpickerview.ColorEnvelope
+import com.skydoves.colorpickerview.ColorPickerDialog
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var selectedImages = mutableListOf<Uri>()
+    private var selectedColors = mutableListOf<Int>()
+    private val productsStorage = Firebase.storage.reference
+    private val firestore = Firebase.firestore
+    private val selectImagesActivityResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
 
+            if (result.resultCode == RESULT_OK) {
+                val intent = result.data
+
+                if (intent?.clipData != null) {
+                    val count = intent.clipData?.itemCount ?: 0
+                    (0 until count).forEach {
+                        val imageUri = intent.clipData?.getItemAt(it)?.uri
+                        imageUri?.let { uri ->
+                            selectedImages.add(uri)
+                        }
+                    }
+                } else {
+                    val imageUri = intent?.data
+                    imageUri?.let {
+                        selectedImages.add(it)
+                    }
+                }
+                updateImages()
+            }
+        }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.buttonColorPicker.setOnClickListener {
+            ColorPickerDialog.Builder(this)
+                .setTitle("Product Color")
+                .setPositiveButton("Select", object : ColorEnvelopeListener {
+                    override fun onColorSelected(envelope: ColorEnvelope?, fromUser: Boolean) {
+                        envelope?.let {
+                            selectedColors.add(it.color)
+                            updateColors()
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel") { colorPicker, _ ->
+                    colorPicker.dismiss()
+                }.show()
+        }
+
+        binding.buttonImagesPicker.setOnClickListener {
+            checkAndRequestPermission()  // Check and request permission before opening image picker
+        }
     }
 
+    private fun openImagePicker() {
+        val intent = Intent(ACTION_PICK).apply {
+            type = "image/*"
+            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        }
+        selectImagesActivityResult.launch(intent)
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission is granted, proceed with image selection
+                openImagePicker()
+            } else {
+                // Handle the case when permission is denied
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun checkAndRequestPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_MEDIA_IMAGES)
+            == PackageManager.PERMISSION_GRANTED) {
+            openImagePicker()  // Permission is already granted, open image picker
+        } else {
+            // Request permission
+            requestPermissionLauncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES)
+        }
+    }
+
+    private fun updateImages() {
+        binding.tvSelectedImages.text = selectedImages.size.toString()
+    }
+
+    private fun updateColors() {
+        var colors = ""
+        selectedColors.forEach {
+            colors = "$colors ${Integer.toHexString(it)}"
+        }
+        binding.productSelectedColors.text = colors
+    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
@@ -22,6 +137,108 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.saveProduct) {
+            val productValidation = validateInformation()
+            if(!productValidation) {
+                Toast.makeText(this, "Check your inputs", Toast.LENGTH_SHORT).show()
+                return false
+            }
+            saveProduct()
+        }
         return super.onOptionsItemSelected(item)
     }
+
+    private fun saveProduct() {
+        val name = binding.productName.text.toString().trim()
+        val category = binding.productCategory.text.toString().trim()
+        val price = binding.productPrice.text.toString().trim()
+        val description = binding.productDescription.text.toString().trim()
+        val offerPercentage = binding.offerPercentage.text.toString().trim()
+        val sizes = getSizesList(binding.productSizes.text.toString().trim())
+        val imagesByteArray = getImagesByteArrays()
+        val images = mutableListOf<String>()
+
+        lifecycleScope.launch(Dispatchers.IO){
+            withContext(Dispatchers.Main){
+                showLoading()
+            }
+            try{
+                async {
+                    imagesByteArray.forEach {
+                        val id = UUID.randomUUID().toString()
+                        launch {
+                            val imageStorage = productsStorage.child("products/images/$id")
+                            val result = imageStorage.putBytes(it).await()
+                            val downloadUrl = result.storage.downloadUrl.await().toString()
+                            images.add(downloadUrl)
+                        }
+                    }
+                }.await()
+            }catch (e: java.lang.Exception){
+                e.printStackTrace()
+                withContext(Dispatchers.Main){
+                    hideLoading()
+                }
+            }
+
+            val product = Product(
+                id = UUID.randomUUID().toString(),
+                name = name,
+                category = category,
+                price = price.toFloat(),
+                offerPercentage = if(offerPercentage.isEmpty()) null else offerPercentage.toFloat(),
+                description = if(description.isEmpty()) null else description,
+                colors = if(selectedColors.isEmpty()) null else selectedColors,
+                sizes = sizes,
+                images = images
+            )
+
+            firestore.collection("Products").add(product).addOnSuccessListener {
+                hideLoading()
+            }.addOnFailureListener {
+                hideLoading()
+                Log.e("Error", it.message.toString())
+            }
+        }
+    }
+
+    private fun hideLoading() {
+        binding.progressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showLoading() {
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun getImagesByteArrays(): List<ByteArray> {
+        val imagesByteArray = mutableListOf<ByteArray>()
+        selectedImages.forEach {
+            val stream = ByteArrayOutputStream()
+            val imageBmp = MediaStore.Images.Media.getBitmap(contentResolver, it)
+            if(imageBmp.compress(Bitmap.CompressFormat.JPEG, 100, stream))
+                imagesByteArray.add(stream.toByteArray())
+        }
+        return imagesByteArray
+    }
+
+    private fun getSizesList(sizesString: String): List<String>? {
+        if(sizesString.isEmpty())
+            return null
+        val sizesList = sizesString.split(",")
+        return sizesList
+    }
+
+    private fun validateInformation(): Boolean{
+        if(binding.productPrice.text.toString().trim().isEmpty())
+            return false
+        if(binding.productName.text.toString().trim().isEmpty())
+            return false
+        if(binding.productCategory.text.toString().trim().isEmpty())
+            return false
+        if(selectedImages.isEmpty())
+            return false
+
+        return true
+    }
+
 }
